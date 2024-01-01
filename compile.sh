1effa4a4bbf5a84fd5df75ebcabec703d5005051
Pastafarista <antonio.cabrera.landin@gmail.com>
2024-01-01 22:26:08 +0100

    build: Movido el fileManager y el broker a sus respectivas carpetas

--- compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Description: Como no se usar CMAKE voy a compilar con un script de bash
-
-g++ cliente.cpp include/* -o output/cliente
-g++ servidor.cpp include/* -o output/servidor
-g++ broker.cpp include/* -o output/broker
